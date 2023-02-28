76e2a97fa5eb1c894844006e67cbec1c9347b958
Ronald <ronaldzamoradev@gmail.com>
2023-02-28 13:24:17 -0500

    feat: add payment test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
-export default function Home () {
+'use client'
+
+import { useEffect } from 'react'
+import { loadStripe } from '@stripe/stripe-js'
+
+import style from './page.module.scss'
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
+
+export default function PreviewPage () {
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search)
+    if (query.get('success')) {
+      console.log('Order placed! You will receive an email confirmation.')
+    }
+
+    if (query.get('canceled')) {
+      console.log('Order canceled -- continue to shop around and checkout when you’re ready.')
+    }
+  }, [])
+
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <form action='/api/checkout_sessions' method='POST' className={style.section}>
+      <button type='submit' role='link' className={style.button}>
+        Checkout
+      </button>
+    </form>
   )
 }
